66509d41ab4937e336739d063e903cb93e389904
Don <donli90@hotmail.com>
2020-03-24 11:13:06 -0700

    stash

--- middleware/token.js
+++ middleware/token.js
@@ -1,25 +1,27 @@
-const { verifyToken } = require('../controller/jwt');
+const { verifyToken } = require("../controller/jwt");
 
 function tokenMiddleWare(req, res, next) {
-  const header = req.headers['x-access-token'] || req.headers['authorization'];
+  // const header = req.headers['x-access-token'] || req.headers['authorization'];
 
-  console.log(req.headers);
-  if (header.startsWith('Bearer ')) {
-    const bearer = header.split(' ');
-    const token = bearer[1];
+  // if (header.startsWith('Bearer ')) {
+  //   const bearer = header.split(' ');
+  // const token = bearer[1];
 
-    if (token) {
-      const payload = verifyToken(token);
-      if (typeof payload !== Error) {
-        req.payload = payload;
-        next();
-      } else {
-        res.render('/pages/error', { msg: 'Invalid token' });
-      };
+  const token = req.cookies.jwt;
+  if (token) {
+    const payload = verifyToken(token);
+    if (typeof payload !== Error) {
+      req.payload = payload;
+      next();
     } else {
-      res.render('/pages/error', { msg: 'No auth token supplied' });
-    };
+      res.problem = "Invalid token";
+      next();
+    }
+  } else {
+    res.problem = "No auth token supplied";
+    next();
   }
-};
+  // }
+}
 
-module.exports = { tokenMiddleWare }
+module.exports = { tokenMiddleWare };
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const dotenv = require('dotenv');
-const database = require('./mysqlDatabase');
+const dotenv = require("dotenv");
+const database = require("./mysqlDatabase");
 const result = dotenv.config();
 
-if (result.error) { throw result.error } else { console.log(result.parsed) };
+if (result.error) {
+  throw result.error;
+} else {
+  console.log(result.parsed);
+}
 const PORT = process.env.port;
 
-database.connect((err) => {
+database.connect(err => {
   if (err) {
     throw err;
   }
 });
 
-const app = require('./app')(database);
+const app = require("./app")(database);
 app.listen(PORT, () => console.log(`Running on : http://localhost:${PORT}`));
