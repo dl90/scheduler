4297c7ab583fe681654ba9f4e7895d306341948d
Don <donli90@hotmail.com>
2020-03-24 11:20:57 -0700

    jwt login

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const { tokenMiddleWare } = require('./middleware/token');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { tokenMiddleWare } = require("./middleware/token");
 const app = express();
 
-module.exports = function (db) {
+module.exports = function(db) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.set('view engine', 'ejs');
+  app.use(cookieParser());
+  app.set("view engine", "ejs");
 
-  app.get('/', (req, res) => {
+  app.get("/", (req, res) => {
     res.render("pages/landing-page", { msg: "Welcome stranger" });
   });
 
   // auth route
-  const auth_route = require('./routes/auth')(db);
-  app.use('/auth', auth_route);
+  const auth_route = require("./routes/auth")(db);
+  app.use("/auth", auth_route);
 
-  app.get('/secure/console', (req, res, next) => {
-    tokenMiddleWare(req, res, next);
-    res.render('pages/console');
+  app.get("/secure/console", tokenMiddleWare, (req, res, next) => {
+    if (res.problem) {
+      res.render("pages/error", { msg: res.problem });
+    } else {
+      res.render("pages/console");
+    }
   });
 
   // api message route
-  const api_route = require('./routes/api')(db);
-  app.use('/api/v1', tokenMiddleWare, api_route);
-
+  const api_route = require("./routes/api")(db);
+  app.use("/api/v1", tokenMiddleWare, api_route);
 
   return app;
-}
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,71 +1,93 @@
-require('dotenv').config();
-const express = require('express');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const { generateToken } = require('../controller/jwt')
+const { generateToken } = require("../controller/jwt");
 const salt = parseInt(process.env.salt);
 
-module.exports = function (db) {
-
+module.exports = function(db) {
   // login
-  router.post('/login', (req, res) => {
+  router.post("/login", (req, res) => {
     const [username, password] = [req.body.username, req.body.password];
 
     db.getUser((err, result) => {
       if (err) {
-        res.render('pages/error', { msg: 'Database error' });
+        res.render("pages/error", { msg: "Database error" });
       } else if (result.length === 1) {
         db.getPassword((err, result) => {
           if (err) {
-            res.render('pages/error', { msg: 'Database error' });
+            res.render("pages/error", { msg: "Database error" });
           } else if (result.length === 1) {
-            bcrypt.compare(password, result[0].password, function (err, result) {
+            bcrypt.compare(password, result[0].password, function(err, result) {
               if (err) {
-                res.render('pages/error', { msg: 'Bcrypt error' });
-              } else if (result) {
+                res.render("pages/error", { msg: "Bcrypt error" });
+              }
+              if (result) {
                 const token = generateToken({ username });
-                res.set({ Authorization: "Bearer " + token });
-                res.send({ msg: 'ok' })
-                // res.redirect('/secure/console');
+                // res.set({ Authorization: "Bearer " + token });
+                // res.send({ msg: 'ok' })
+                return res
+                  .cookie("jwt", token, { httpOnly: false, sameSite: true })
+                  .redirect("/secure/console");
               } else {
-                res.render('pages/error', { msg: 'Incorrect login info' });
-              };
+                res.render("pages/error", { msg: "Incorrect login info" });
+              }
             });
           }
         }, username);
       } else {
-        res.render('pages/error', { msg: 'Incorrect login info' });
-      };
+        res.render("pages/error", { msg: "Incorrect login info" });
+      }
     }, username);
   });
 
   // signup
-  router.post('/sign-up', (req, res) => {
-    const [username, password, email] = [req.body.new_username, req.body.new_password, req.body.new_email];
+  router.post("/sign-up", (req, res) => {
+    const [username, password, email] = [
+      req.body.new_username,
+      req.body.new_password,
+      req.body.new_email
+    ];
 
     db.getUser((err, result) => {
       if (err) {
-        res.render('pages/error', { msg: 'Database error' });
+        res.render("pages/error", { msg: "Database error" });
       } else if (result.length > 0) {
-        res.render('pages/error', { msg: 'User already exists' });
+        res.render("pages/error", { msg: "User already exists" });
       } else {
-        bcrypt.hash(password, salt, function (err, hash) {
+        bcrypt.hash(password, salt, function(err, hash) {
           if (err) {
-            res.render('pages/error', { msg: 'Bcrypt' });
+            res.render("pages/error", { msg: "Bcrypt" });
           } else {
-            db.createUser((err, result) => {
-              if (err) {
-                res.render('pages/error', { msg: 'Database Error' });
-              } else {
-                res.render('/pages/landing-page', { msg: 'User successfully created.' });
-              };
-              console.log('New user registered with user id: ' + result.insertId);
-            }, username, hash, email);
-          };
+            db.createUser(
+              (err, result) => {
+                if (err) {
+                  res.render("pages/error", { msg: "Database Error" });
+                } else {
+                  res.render("/pages/landing-page", {
+                    msg: "User successfully created."
+                  });
+                }
+                console.log(
+                  "New user registered with user id: " + result.insertId
+                );
+              },
+              username,
+              hash,
+              email
+            );
+          }
         });
-      };
+      }
     }, username);
   });
 
+  router.get("/logout", (req, res) => {
+    const token = req.cookies.jwt;
+    if (token) {
+      res.clearCookie("jwt", { httpOnly: false, sameSite: true }).redirect("/");
+    }
+  });
+
   return router;
-}
+};
